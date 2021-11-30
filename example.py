e5f52491759cfffadedb123665ec93b0c7072856
Mitchel Stephan <mitchel@stephanclan.com>
2021-11-30 11:26:10 -0700

    main

--- example.py
+++ example.py
@@ -1,14 +1,27 @@
-#!/usr/env python 
+#!/usr/env python
 
 #  Description: This file does something
 #  Author: Mitchel Stephan
 #  LastUpdated: 11/30/2021
 
+import sys
+import time
+
 # Function Definition
-def main():
-    print("does this thing work")
+
+
+def main(args):
+    #  DO SOME WORK
+
+    total = 0
+    for item in args[1:]:
+        total += int(item) 
+        # time.sleep(.25)
+        # print("  [~] running total: ",total)
+
+    print("[+] The sum is", total)
 
 
 # Function Execution
 if __name__ == '__main__':
-    main()
+    main(sys.argv)

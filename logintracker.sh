ea391cff587ba78ff9a7053ab8e04da5a85bf07a
Mitchel Stephan <mitchel@stephanclan.com>
2021-12-01 14:08:41 -0700

    main

--- logintracker.sh
+++ logintracker.sh
@@ -2,7 +2,8 @@
 
 getloginattempts(){
   attemptlist= Get-Winevent -MaxEvents 100 -LogName Security | Where-Object {$_.ID -eq 4624 -or $_.ID -eq 4625}
+  echo $attemptlist >> loginhistory.txt
 }
 
+
 getloginattempts()
-echo $attemptlist >> loginhistory.txt
